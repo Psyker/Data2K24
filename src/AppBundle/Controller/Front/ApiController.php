e7ecc137e1a66e8af247d3f3bcaba42ded893f23
Psyker <bourgoin.theo1@gmail.com>
2018-01-22 23:13:17 +0100

    wip

--- src/AppBundle/Controller/Front/ApiController.php
+++ src/AppBundle/Controller/Front/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Front;
 
+use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
